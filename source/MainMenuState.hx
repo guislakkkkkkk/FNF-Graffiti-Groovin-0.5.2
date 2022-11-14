a89155c33bd7b8cb43fa0a5bf1f037436d78fc89
guislakkkkkkk <107629042+guislakkkkkkk@users.noreply.github.com>
2022-11-14 15:22:32 -0300

    f

--- source/MainMenuState.hx
+++ source/MainMenuState.hx
@@ -68,7 +68,7 @@ class MainMenuState extends MusicBeatState
 		persistentUpdate = persistentDraw = true;
 
 		var yScroll:Float = Math.max(0.25 - (0.05 * (optionShit.length - 4)), 0.1);
-		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('renders/skarlet'));
+		var bg:FlxSprite = new FlxSprite(-80).loadGraphic(Paths.image('skarlet'));
 		bg.scrollFactor.set(0, yScroll);
 		bg.setGraphicSize(Std.int(bg.width * 1.175));
 		bg.x = 60;
